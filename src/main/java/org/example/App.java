81d7912be3261dcf11867567f0c92eccef3a33d3
TULASI192 <tulasigovada77@gmail.com>
2026-04-08 11:04:24 +0530

    Tulasi comitted UC2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,21 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+
+        public static void main(String[] args) {
+
+            if (args.length == 0) {
+                System.out.println("No arguments given.");
+                return;
+            }
+
+            System.out.println("Names entered are:");
+
+            for (int i = 0; i < args.length; i++) {
+                System.out.println(args[i]);
+            }
+        }
     }
-}
