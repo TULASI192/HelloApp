3d2ffa17c9058df87edfa30add1bfcf0190178f5
TULASI192 <tulasigovada77@gmail.com>
2026-04-11 01:58:45 +0530

    Tulasi committed UC7

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,21 +1,21 @@
 package org.example;
 public class App {
     public static void main(String[] args) {
-
+        // Case 1: No arguments default message
         if (args.length == 0) {
             System.out.println("Hello, World!");
             return;
         }
 
-
+        // Use StringBuilder for efficiency
         StringBuilder nameBuilder = new StringBuilder();
 
-
+        // Append all names with ","
         for (String name : args) {
             nameBuilder.append(name).append(", ");
         }
 
-
+        // Remove trailing ", "
         if (nameBuilder.length() > 0) {
             nameBuilder = new StringBuilder(
                     nameBuilder.substring(0, nameBuilder.length() - 2));
