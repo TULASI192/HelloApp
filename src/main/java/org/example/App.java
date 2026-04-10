0cbc6cbadd8aa2bd8fff0a07aa52cf3669832503
TULASI192 <tulasigovada77@gmail.com>
2026-04-11 01:52:26 +0530

    Tulasi committed UC6

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,21 +1,24 @@
 package org.example;
-
 public class App {
     public static void main(String[] args) {
-        // If no arguments provided, print default message
+
         if (args.length == 0) {
             System.out.println("Hello, World!");
             return;
         }
-        // Build comma-separated names using StringBuilder
-        StringBuilder namesBuilder = new StringBuilder();
-        boolean first = true;
-        for (String name: args) {
-            if (!first) {
-                namesBuilder.append(", ");
-            }
-            namesBuilder.append(name);
-            first = false;
+
+
+        StringBuilder nameBuilder = new StringBuilder();
+
+
+        for (String name : args) {
+            nameBuilder.append(name).append(", ");
+        }
+
+
+        if (nameBuilder.length() > 0) {
+            nameBuilder = new StringBuilder(
+                    nameBuilder.substring(0, nameBuilder.length() - 2));
         }
     }
 }
